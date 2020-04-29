86318a240a801286484386bf96777f984b304553
Uriel <ur13l.infante@gmail.com>
2020-04-29 18:39:31 -0500

    [UPDATE] Remove node_modules

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const yf = require("yahoo-finance");
 const fs = require("fs");
-const AWS = require('aws-sdk');
+const AWS = require("aws-sdk");
+const dotenv = require('dotenv');
+dotenv.config();
+
+console.log(process.env.AWS_ACCESS_KEY);
+console.log(process.env.AWS_SECRET_ACCESS_KEY);
 const s3 = new AWS.S3({
-    accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-})
+  accessKeyId: 'AKIAQK37GNI7H4U6TBEB',
+  secretAccessKey: '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'
+});
 
 const SYMBOLS = [
-  "MEX-TIIE28.MX",
-  "MEX-CETES-I.MX",
   "USDMXN=X",
   "^MXX",
   "^NYA",
@@ -20,31 +23,28 @@ const SYMBOLS = [
   "ETH-USD"
 ];
 
-exports.handler = async event => {
+exports.handler = (event) => {
   yf.quote({
     fields: ["a", "b", "b2", "b3", "p", "o"],
     symbols: SYMBOLS
   })
     .then(response => {
       return new Promise((resolve, reject) => {
-        fs.writeFile("prices.json", JSON.stringify(response), err => {
-          if (err) reject(err);
-          else resolve(response);
-        });
+
+        const params = {
+            Body: JSON.stringify(response, null, 2),
+            Bucket: "hub.rocktech",
+            Key: "prices.json",
+          };
+          s3.upload(params, (s3Err, data) => {
+            if (s3Err) throw s3Err;
+            console.log("File uploaded successfully");
+          });
       });
-    })
-    .then(results => {
-      console.log("Results: " + results);
     })
     .catch(err => {
       console.log("Error: " + err);
     });
 
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({ success: true })
-  };
-  return response;
+    return "DONE"
 };
-
-exports.handler();
